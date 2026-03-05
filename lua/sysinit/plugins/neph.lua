56575efc89b5335c43a3e091b333e7fbc63a8917
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-04 21:14:08 -0800

    wip

---
+++ lua/sysinit/plugins/neph.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "roshbhatia/neph.nvim",
+    dependencies = {
+      "folke/snacks.nvim",
+      -- "nvim-treesitter/nvim-treesitter",
+      -- "Saghen/blink.cmp",
+    },
+    opts = {},
+    keys = function()
+      local api = require("neph.api")
+      return {
+        { "<leader>jj", api.toggle, desc = "Neph: toggle / pick agent" },
+        { "<leader>jJ", api.kill_and_pick, desc = "Neph: kill session & pick new" },
+        { "<leader>jx", api.kill, desc = "Neph: kill active session" },
+        { "<leader>ja", api.ask, mode = { "n", "v" }, desc = "Neph: ask active" },
+        { "<leader>jf", api.fix, desc = "Neph: fix diagnostics" },
+        { "<leader>jc", api.comment, mode = { "n", "v" }, desc = "Neph: comment" },
+        { "<leader>jv", api.resend, desc = "Neph: resend previous prompt" },
+        { "<leader>jh", api.history, desc = "Neph: browse prompt history" },
+      }
+    end,
+  },
+}
