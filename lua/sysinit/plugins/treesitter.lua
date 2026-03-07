b93d9abaafef81cfbf8a3e4c98f1d22c70a5e1c8
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-07 10:36:15 -0800

    tt

--- lua/sysinit/plugins/treesitter.lua
+++ lua/sysinit/plugins/treesitter.lua
@@ -93,10 +93,10 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "gx",
-            node_incremental = "gx",
-            scope_incremental = false,
-            node_decremental = "gX",
+            init_selection = "vvv",
+            node_incremental = "vvi",
+            scope_incremental = "vvI",
+            node_decremental = "vvd",
           },
         },
         indent = {
