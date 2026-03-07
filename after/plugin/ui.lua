20a0010d2d9decf025ef5d34843cfdf2097a680e
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-07 10:45:43 -0800

    showtabline

--- after/plugin/ui.lua
+++ after/plugin/ui.lua
@@ -2,7 +2,7 @@ vim.opt.laststatus = 3
 vim.opt.pumblend = 15
 vim.opt.shortmess:append("sIWc")
 vim.opt.showmode = false
-vim.opt.showtabline = 0
+vim.opt.showtabline = 2
 vim.opt.sidescrolloff = 0
 vim.opt.splitkeep = "screen"
 vim.opt.termguicolors = true
