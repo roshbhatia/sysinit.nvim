82c1b71e6c2be8aa8a4710dac965cf177ffdd53e
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-13 14:21:45 -0700

    remove leader ee global toggle

--- lua/sysinit/plugins/edgy.lua
+++ lua/sysinit/plugins/edgy.lua
@@ -2,6 +2,56 @@ return {
   {
     "folke/edgy.nvim",
     event = "VeryLazy",
+    keys = {
+      {
+        "<leader>et",
+        function()
+          vim.cmd("Neotree toggle")
+          vim.cmd("wincmd p")
+        end,
+        desc = "Toggle explorer tree",
+      },
+      {
+        "<leader>ex",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Toggle diagnostics (Trouble)",
+      },
+      {
+        "<leader>eg",
+        function()
+          require("grug-far").toggle_instance({
+            instanceName = "far-global",
+            staticTitle = "Global Search",
+            prefills = {
+              search = vim.fn.expand("<cword>"),
+              filesFilter = "*",
+            },
+          })
+        end,
+        desc = "Toggle global search (Grug-far)",
+      },
+      {
+        "<leader>eo",
+        function()
+          vim.cmd("Outline!")
+        end,
+        desc = "Toggle outline",
+      },
+      {
+        "]e",
+        function()
+          require("edgy").next()
+        end,
+        desc = "Next sidebar",
+      },
+      {
+        "[e",
+        function()
+          require("edgy").prev()
+        end,
+        desc = "Prev sidebar",
+      },
+    },
     opts = {
       animate = {
         enabled = false,
@@ -51,6 +101,18 @@ return {
           filter = function(buf)
             return vim.bo[buf].buftype == "help"
           end,
+        },
+        {
+          ft = "Avante",
+          size = { width = 0.4 },
+        },
+        {
+          ft = "AvanteInput",
+          size = { width = 0.4 },
+        },
+        {
+          ft = "AvanteSelectedFiles",
+          size = { width = 0.4 },
         },
       },
     },
