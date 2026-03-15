6ee8fc117a46ce6d3caa91b6f3f9eb6201a006eb
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-14 20:44:08 -0700

    no auto sync

--- lua/sysinit/plugins/treesitter.lua
+++ lua/sysinit/plugins/treesitter.lua
@@ -61,7 +61,7 @@ return {
           "yaml",
         },
         sync_install = false,
-        auto_install = true,
+        auto_install = false,
         ignore_install = {
           "org",
         },
