4ef723df602208586e35a6d655799f2febad0bea
Roshan Bhatia <rshnbhatia@gmail.com>
2026-05-27 11:34:55 -0700

    wip

--- lua/plugins/colorful-winsep.lua
+++ lua/plugins/colorful-winsep.lua
@@ -1,7 +1,14 @@
 return {
   {
     "nvim-zh/colorful-winsep.nvim",
-    config = true,
+    config = function()
+      require("colorful-winsep").setup({
+        border = "rounded",
+        animate = {
+          enabled = "false",
+        },
+      })
+    end,
     event = { "WinLeave" },
   },
 }
